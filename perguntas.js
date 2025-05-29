fba0f1c4280e535a4a6bb36cd7cc2ac2dd7cb9da
Crislaine77 <crislaine.santos.barbosa@escola.pr.gov.br>
2025-05-28 22:32:04 -0300

    28/05

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartão(
+    "programação" 
+    "o que é Pyton?"
+    "o Pyton é uma linguagem de programação"
+)
+
+criaCartão(
+  "Geografia"
+  "qual a capital da França?"
+  " A capital da França é Paris"
+)
+
+criaCartão(
+    "programação"
+    "o que é uma função?"
+    " Uma função é o bloco de um codigo que executa alguma tarefa"
+)
+  
+criaCartão (
+    "Programação"
+    " O que é JavaScript?",
+    " É uma limguagem de programação"
+)
